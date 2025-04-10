8c226527b9221b4152b13889d7d76089fd085e0f
Emils <emils@gmail.com>
2025-04-10 15:46:38 +0300

    postpolicijā izlaboju delete funkciju

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -46,6 +46,6 @@ class PostPolicy
      */
     public function delete(User $user, Post $post): bool
     {
-        return $user->id !== $post->user_id;
+        return $user->id === $post->user_id;
     }
 }
